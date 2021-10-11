a7605417690c896484545ca98a3653d18eaa166a
Stefano Le Pera <stefanolepera@192.168.1.109>
2021-10-11 21:59:15 +0100

    changed swapi endpoint

--- src/constants/APIEndPoints.js
+++ src/constants/APIEndPoints.js
@@ -1,8 +1,8 @@
 export const API_END_POINTS = {
-    Films: 'https://swapi.co/api/films/',
-    Characters: 'https://swapi.co/api/people/?search=',
-    Starships: 'https://swapi.co/api/starships/?search=',
-    Vehicles: 'https://swapi.co/api/vehicles/?search=',
-    Species: 'https://swapi.co/api/species/?search=',
-    Planets: 'https://swapi.co/api/planets/?search=',
+    Films: 'https://swapi.dev/api/films/',
+    Characters: 'https://swapi.dev/api/people/?search=',
+    Starships: 'https://swapi.dev/api/starships/?search=',
+    Vehicles: 'https://swapi.dev/api/vehicles/?search=',
+    Species: 'https://swapi.dev/api/species/?search=',
+    Planets: 'https://swapi.dev/api/planets/?search=',
 }
